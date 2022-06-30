e94c7e2caad60c7b27716c543aaf54db7d61914c
Allyson1602 <aathyrson@yahoo.com.br>
2022-06-30 10:51:36 -0300

    Implementação de layout responsive na página inicial

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -28,20 +28,22 @@ export function Subscribe() {
     }
 
     return (
-        <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-                <div className="max-w-[640px]">
-                    <Logo />
+        <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center md:px-8">
+            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto flex-col md:flex-row">
+                <div className="max-w-[640px] md:pr-4 text-center md:text-start">
+                    <header className="flex justify-center md:justify-start">
+                        <Logo />
+                    </header>
 
-                    <h1 className="mt-8 text-[2.5rem] leading-tight">
+                    <h1 className="mt-8 leading-tight text-3xl md:text-[2.5rem] md:px-0">
                         Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
                     </h1>
-                    <p className="mt-4 text-gray-200 leading-relaxed">
+                    <p className="mt-4 text-gray-200 leading-relaxed text-sm md:text-base px-5 md:px-0">
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
                 </div>
 
-                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+                <div className="p-8 bg-gray-700 border border-gray-500 w-full md:w-auto mt-8 md:mt-0 md:rounded">
                     <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
                     <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
@@ -70,7 +72,7 @@ export function Subscribe() {
                 </div>
             </div>
 
-            <img src={codeMockup} className="mt-10" alt="" />
+            <img src={codeMockup} className="mt-10 p-2" alt="" />
         </div>
     );
 }
